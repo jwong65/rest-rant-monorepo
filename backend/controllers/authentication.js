73879311bbde278f1f2f7b827ae2eb0a07dff980
jwong65 <jason.wong6507@gmail.com>
2023-04-01 21:32:22 -0400

    Fixed router to not have 2 POST and fixed both forms

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -4,10 +4,6 @@ const bcrypt = require('bcrypt')
 
 const { User } = db
 
-router.post('/', async (req, res) => {
-    console.log('IN HERE')
-})
-
 router.post('/', async (req, res)=>{
     let user = await User.findOne({
         where: { email: req.body.email}
