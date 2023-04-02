1aecd55ba468613e0e2c8921d2918f1f435d7a87
jwong65 <jason.wong6507@gmail.com>
2023-04-01 21:11:41 -0400

    Added route in the backend for bcrypt in assignment

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -8,4 +8,20 @@ router.post('/', async (req, res) => {
     console.log('IN HERE')
 })
 
+router.post('/', async (req, res)=>{
+    let user = await User.findOne({
+        where: { email: req.body.email}
+    })
+    console.log(user)
+    
+    if (!user || !await bcrypt.compare(req.body.password, user.passwordDigest)) {
+        res.status(404).json({ 
+            message: `Could not find a user with the provided username and password` 
+        })
+    } else {
+        res.json({ user })
+    }
+
+})
+
 module.exports = router
